4a28401837b073cd209aa95a04449a6326e13519
PierreExeter <pierre.aumjaud@gmail.com>
2020-05-18 13:33:52 +0200

    adding plotting (not finished)

--- tests/6_test_pytorch.py
+++ tests/6_test_pytorch.py
@@ -4,3 +4,7 @@ import torch
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 print(device)
+
+x = torch.rand(5, 3)
+print(x)
+
--- tests/eof_test.py
+++
@@ -1,9 +0,0 @@
-try:
-    while True:
-        data = raw_input('prompt:')
-        print('READ:', data)
-    
-except EOFError as e:
-    print(e)
-
-# $ echo hello | python eof_test.py
