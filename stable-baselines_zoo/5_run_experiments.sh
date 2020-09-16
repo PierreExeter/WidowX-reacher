ac9baf2b3e0e8dafa2e917942ba5909ed20261e4
PierreExeter <pierre.aumjaud@gmail.com>
2020-09-16 21:00:33 +0200

    ready to train on KAY

--- stable-baselines_zoo/5_run_experiments.sh
+++ stable-baselines_zoo/5_run_experiments.sh
@@ -3,9 +3,9 @@
 # experiments over 10 initialisation seeds
 
 # nsteps=10000     # NEW: don't specify explicitely the number of training steps, rather use the value in hyperparams/algo.yml (easier to log the hyperparameters afterwards)
-nsteps_rand=10000
-log_dir="logs/train_10K_widowx_reacher-v5"
-log_dir_rand="logs/train_10K_widowx_reacher-v5_random"
+nsteps_rand=500000
+log_dir="logs/train_0.5M_widowx_reacher-v5_bench"
+log_dir_rand="logs/train_0.5M_widowx_reacher-v5_random"
 env="widowx_reacher-v5"
 env_her="widowx_reacher-v6"
 nseeds=2
@@ -13,26 +13,26 @@ nseeds=2
 
 for ((i=0;i<${nseeds};i+=1))
 do
-    # echo "A2C $i"
-    # python3 2_train.py --algo a2c --env ${env} --seed $i --log-folder ${log_dir} &> submission_log/log_a2c_0$i.run
+    echo "A2C $i"
+    python3 2_train.py --algo a2c --env ${env} --seed $i --log-folder ${log_dir} &> submission_log/log_a2c_0$i.run
 
-    # echo "ACKTR $i"
-    # python3 2_train.py --algo acktr --env ${env} --seed $i --log-folder ${log_dir} &> submission_log/log_acktr_0$i.run
+    echo "ACKTR $i"
+    python3 2_train.py --algo acktr --env ${env} --seed $i --log-folder ${log_dir} &> submission_log/log_acktr_0$i.run
 
-    # echo "DDPG $i"
-    # python3 2_train.py --algo ddpg --env ${env} --seed $i --log-folder ${log_dir} &> submission_log/log_ddpg_0$i.run
+    echo "DDPG $i"
+    python3 2_train.py --algo ddpg --env ${env} --seed $i --log-folder ${log_dir} &> submission_log/log_ddpg_0$i.run
 
-    # echo "PPO2 $i"
-    # python3 2_train.py --algo ppo2 --env ${env} --seed $i --log-folder ${log_dir} &> submission_log/log_ppo2_0$i.run
+    echo "PPO2 $i"
+    python3 2_train.py --algo ppo2 --env ${env} --seed $i --log-folder ${log_dir} &> submission_log/log_ppo2_0$i.run
 
-    # echo "SAC $i"
-    # python3 2_train.py --algo sac --env ${env} --seed $i --log-folder ${log_dir} &> submission_log/log_sac_0$i.run
+    echo "SAC $i"
+    python3 2_train.py --algo sac --env ${env} --seed $i --log-folder ${log_dir} &> submission_log/log_sac_0$i.run
 
-    # echo "TD3 $i"
-    # python3 2_train.py --algo td3 --env ${env} --seed $i --log-folder ${log_dir} &> submission_log/log_td3_0$i.run
+    echo "TD3 $i"
+    python3 2_train.py --algo td3 --env ${env} --seed $i --log-folder ${log_dir} &> submission_log/log_td3_0$i.run
 
-    # echo "TRPO $i"
-    # python3 2_train.py --algo trpo --env ${env} --seed $i --log-folder ${log_dir} &> submission_log/log_trpo_0$i.run
+    echo "TRPO $i"
+    python3 2_train.py --algo trpo --env ${env} --seed $i --log-folder ${log_dir} &> submission_log/log_trpo_0$i.run
 
     echo "HER $i"
     python3 2_train.py --algo her --env ${env_her} --seed $i --log-folder ${log_dir} &> submission_log/log_her_sac_0$i.run
